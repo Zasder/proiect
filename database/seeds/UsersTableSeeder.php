ddb2c922b636695785d44dcce3348c66160950f6
Zasder <danfrigioiu@gmail.com>
2018-12-05 14:15:29 +0200

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Model\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //create a test user
+        factory(User::class)->create(['name'=>'Test User','email'=>'user@example.com']);
+    
+    	//create 50 random test users
+    	factory(User::class,50)->create();
+	}
+}
